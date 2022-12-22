b9377f17e77496f80a9ee637ca6e64f128ce28da
Kyle Coffey <me@phytolizer.dev>
2022-12-22 08:55:54 -0600

    fix unlines

--- src/initial/unlines.zig
+++ src/initial/unlines.zig
@@ -5,7 +5,9 @@ pub fn unlines(a: std.mem.Allocator, lines: Lines) ![]u8 {
     var buf = std.ArrayList(u8).init(a);
     defer buf.deinit();
     for (lines.inner.items) |line| {
-        try buf.appendSlice(line.items);
+        const nontrivial = try line.getNonTrivial(a);
+        defer a.free(nontrivial);
+        try buf.appendSlice(nontrivial);
     }
     lines.deinit();
     return try buf.toOwnedSlice();
